1d65ee6a0d1b04c537634361a3bb1d690b2b88d1
Dima <krasti-vip@yandex.ru>
2024-11-11 17:59:25 +0200

    STEP 29 Fix
    STEP 29 Registration Game

--- src/game/person/Hero.java
+++ src/game/person/Hero.java
@@ -8,9 +8,27 @@ public abstract class Hero<T extends Weapon> {
      */
     protected T weapon;
 
-    public Hero(T weapon) {
+    private int hp;
+
+    private int mana;
+
+    private int power;
+
+    private int intelligence;
+
+    private int dexterity;
+
+    public Hero(T weapon, int hp, int mana, int power, int intelligence, int dexterity) {
         this.weapon = weapon;
+        this.hp = hp;
+        this.mana = mana;
+        this.power = power;
+        this.intelligence = intelligence;
+        this.dexterity = dexterity;
     }
 
+    /**
+     * тут нахерачить характеристики ману жизнь урон атаку 
+     */
     abstract public void attack();
 }
